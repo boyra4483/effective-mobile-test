d193e32362b361c95800a05b61e13eea0e21eb6c
boyra <b9892213@gmail.com>
2025-08-27 20:17:58 +0300

    create movie and movies types

---
+++ src/types/movies.ts
@@ -0,0 +1,11 @@
+export type Movies = Movie[];
+
+export interface Movie {
+	name: string;
+	description: string;
+	year: number;
+	rating: { kp: number };
+	poster: { url: string; previewUrl: string };
+	logo: { url: string; previewUrl: string };
+	genres: { name: string }[];
+}

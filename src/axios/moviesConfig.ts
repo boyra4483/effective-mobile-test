c6680c2fdf057004cb6c1bb87c0c7d4716d5c5ad
boyra <b9892213@gmail.com>
2025-08-27 20:14:28 +0300

    create transformer fn to transform movie data

--- src/axios/moviesConfig.ts
+++ src/axios/moviesConfig.ts
@@ -1,11 +1,19 @@
 import { type AxiosRequestConfig } from "axios";
+import { type Movies } from "../types/movies";
 
 const moviesConfig: AxiosRequestConfig = {
 	baseURL: "https://api.kinopoisk.dev/v1.4/movie/search?page=1&query=",
 	headers: {
 		"X-API-KEY": "QX6HE6Z-W6N4ACK-JDJQ1BE-0MC5FQ5"
 	},
-	method: "GET"
+	method: "GET",
+	transformResponse: transformer
 };
+
+function transformer(data: string) {
+	const parsedData = JSON.parse(data);
+	const movies: Movies = parsedData.docs;
+	return movies;
+}
 
 export default moviesConfig;

b1bac4dd4963d9f10446489368e2c577d2fced00
boyra <b9892213@gmail.com>
2025-08-27 20:20:05 +0300

    create header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import styles from "./header.module.scss";
+
+interface HeaderProps {
+	children: ReactNode;
+}
+
+export default function Header({ children }: HeaderProps) {
+	return (
+		<header className={styles["header"]}>
+			<h1 className={styles["title"]}>Каталог фильмов</h1>
+			{children}
+		</header>
+	);
+}

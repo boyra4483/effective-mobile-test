160bdc7a9ac937a13e3ed1e2d67acbcdf6a5a943
boyra <b9892213@gmail.com>
2025-08-28 03:37:41 +0300

    implement infinite scrolling to load more movies as the user scrolls down the list.
    1 - use "useEffect" hook to fetch initial movies on component mount.
    2 - add "page" state for pagination.
    3 - create "handleScroll" function to detect when user scrolls to the
        bottom of the list and fetch next page of movies and append to
        existing list when bottom is reached.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styles from "./app.module.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { type Movies } from "./types/movies";
 import Header from "./components/header/Header";
 import Search from "./components/search/Search";
@@ -9,11 +9,39 @@ import fetchMovies from "./api/fetchMovies";
 export default function App() {
 	const [searchText, setSearchText] = useState<string>("");
 	const [movies, setMovies] = useState<Movies | null>(null);
+	const [page, setPage] = useState<number>(1);
+
+	useEffect(() => {
+		let ignore = false;
+		console.log("fetching movies");
+		(async () => {
+			const movies = await fetchMovies(1, "");
+			if (!ignore) {
+				setMovies(movies);
+			}
+		})();
+
+		return () => {
+			ignore = true;
+		};
+	}, []);
 
 	async function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
 		setSearchText(e.target.value);
 		const movies = await fetchMovies(1, e.target.value);
 		setMovies(movies);
+	}
+
+	async function handleScroll(e: React.UIEvent<HTMLUListElement>) {
+		const currentTarget = e.currentTarget;
+		if (
+			currentTarget.scrollTop + currentTarget.clientHeight >=
+			currentTarget.scrollHeight - 20
+		) {
+			const res = await fetchMovies(page + 1, "");
+			setMovies([...movies!, ...res]);
+			setPage(page + 1);
+		}
 	}
 
 	return (
@@ -22,7 +50,9 @@ export default function App() {
 				<Search value={searchText} onSearch={handleSearch} />
 			</Header>
 			<main className={styles["content"]}>
-				<ul className={styles["movies"]}>{getMovies(movies)}</ul>
+				<ul onScroll={handleScroll} className={styles["movies"]}>
+					{getMovies(movies)}
+				</ul>
 			</main>
 		</>
 	);

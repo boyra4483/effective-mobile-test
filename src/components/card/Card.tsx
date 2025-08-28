d9f032e7de9587e967979754c3eacf52c2e40e27
boyra <b9892213@gmail.com>
2025-08-28 06:26:29 +0300

    change classes in according to BEM methodology

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -8,17 +8,17 @@ interface CardProps {
 
 export default function Card({ src, movieName, year }: CardProps) {
 	return (
-		<figure className={styles["figure"]}>
+		<figure className={styles["card"]}>
 			<img
 				src={src}
 				width={200}
 				height={300}
 				alt="movie poster"
-				className={styles["img"]}
+				className={styles["card__img"]}
 			/>
-			<figcaption className={styles["caption"]}>
-				<h2 className={styles["movie-name"]}>{movieName}</h2>
-				<time className={styles["movie-year"]} dateTime={year}>
+			<figcaption className={styles["card__caption"]}>
+				<h2 className={styles["card__movie-name"]}>{movieName}</h2>
+				<time className={styles["card__movie-year"]} dateTime={year}>
 					{year}
 				</time>
 			</figcaption>

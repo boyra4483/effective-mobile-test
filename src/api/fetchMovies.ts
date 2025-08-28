d71b887e93e477cbe94e43c3b9311abe4a5bbea2
boyra <b9892213@gmail.com>
2025-08-28 06:30:40 +0300

    remove "console.log"

--- src/api/fetchMovies.ts
+++ src/api/fetchMovies.ts
@@ -3,7 +3,6 @@ import { type Movies } from "../types/movies";
 import moviesConfig from "../axios/moviesConfig";
 
 export default async function fetchMovies(page: number = 1, movieName: string) {
-	console.log(page);
 	const moviesRes = await axios<Movies>(
 		`search?page=${page}&limit=10&query=${movieName}`,
 		moviesConfig

73f8917b7925508035d10f78355fc29be1252d2a
boyra <b9892213@gmail.com>
2025-08-28 05:45:38 +0300

    change "baseURL" prop

--- src/axios/moviesConfig.ts
+++ src/axios/moviesConfig.ts
@@ -2,7 +2,7 @@ import { type AxiosRequestConfig } from "axios";
 import { type Movies } from "../types/movies";
 
 const moviesConfig: AxiosRequestConfig = {
-	baseURL: "https://api.kinopoisk.dev/v1.4/movie/search?page=1&query=",
+	baseURL: "https://api.kinopoisk.dev/v1.4/movie",
 	headers: {
 		"X-API-KEY": "QX6HE6Z-W6N4ACK-JDJQ1BE-0MC5FQ5"
 	},
